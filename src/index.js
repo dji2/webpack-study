381271762ee884ca05bcb77089eda721f3b3a5e2
desong <dji2@xiaohongshu.com>
2021-04-07 20:04:20 +0800

    fix: 支持资源配置

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
-import _ from 'lodash'
+import _ from 'lodash';
+import printMe from './print.js';
 
-function component() {
-  const element = document.createElement('div');
+ function component() {
+   const element = document.createElement('div');
+  const btn = document.createElement('button');
 
-  // lodash（目前通过一个 script 引入）对于执行这一行是必需的
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
 
-  return element;
-}
+  btn.innerHTML = 'Click me and check the console!';
+  btn.onclick = printMe;
 
-document.body.appendChild(component());
+  element.appendChild(btn);
+
+   return element;
+ }
+
+ document.body.appendChild(component());
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,30 @@
 const path = require('path');
-
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
-  entry: './src/index.js',
+  entry: {
+    index: './src/index.js',
+    print: './src/print.js',
+  },
   output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].bundle.js',
+     path: path.resolve(__dirname, 'dist'),
+     clean: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: '管理输出',
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+    ],
   },
 };

9b3643a8ed8839d207cfe0d9dd6d640fbd98966c
desong <dji2@xiaohongshu.com>
2021-04-07 20:36:22 +0800

    fix: webpack-bundle-analyzer

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,14 @@
 const path = require('path');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 module.exports = {
   mode: 'development',
   entry: {
     index: './src/index.js',
   },
+  plugins: [
+    new BundleAnalyzerPlugin()
+  ],
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),

2b87227dbe597c4ea5480cf4a32d0959c13dec41
desong <dji2@xiaohongshu.com>
2021-04-07 20:32:03 +0800

    fix: 动态import

--- src/index.js
+++ src/index.js
@@ -1,18 +1,11 @@
-import _ from 'lodash';
-import printMe from './print.js';
 
- function component() {
-   const element = document.createElement('div');
-  const btn = document.createElement('button');
+ async function getComponent() {
+  const element = document.createElement('div');
+  const { default: _ } = await import('lodash');
+  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
 
-   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-
-  btn.innerHTML = 'Click me and check the console!';
-  btn.onclick = printMe;
-
-  element.appendChild(btn);
-
-   return element;
- }
-
- document.body.appendChild(component());
+  return element;
+}
+getComponent().then((component) => {
+  document.body.appendChild(component);
+});
--- webpack.config.js
+++ webpack.config.js
@@ -1,48 +1,12 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
   mode: 'development',
   entry: {
-    index: {
-      import: './src/index.js',
-      dependOn: 'shared',
-    },
-    another: {
-      import: './src/another-module.js',
-      dependOn: 'shared',
-    },
-    shared: 'lodash',
+    index: './src/index.js',
   },
   output: {
     filename: '[name].bundle.js',
-     path: path.resolve(__dirname, 'dist'),
-     clean: true,
-     publicPath: '/',
-  },
-  devtool: 'inline-source-map',
-  devServer: {
-    contentBase: './dist',
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: 'Development',
-    }),
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
-      },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: 'asset/resource',
-      },
-    ],
-  },
-  optimization: {
-    splitChunks: {
-      chunks: 'all',
-    },
+    path: path.resolve(__dirname, 'dist'),
   },
 };

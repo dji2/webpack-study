285a9b10d4a8d9164b1ea8657b09ded8395b5bf6
desong <dji2@xiaohongshu.com>
2021-04-07 20:06:58 +0800

    fix: source map

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
+  mode: 'development',
   entry: {
     index: './src/index.js',
     print: './src/print.js',
@@ -10,9 +11,10 @@ module.exports = {
      path: path.resolve(__dirname, 'dist'),
      clean: true,
   },
+  devtool: 'inline-source-map',
   plugins: [
     new HtmlWebpackPlugin({
-      title: '管理输出',
+      title: 'Development',
     }),
   ],
   module: {
